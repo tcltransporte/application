de56bfb4b8890ea457b8d76a575896ca60c76e53
guilhermetcltransporte <guilherme.venancio@tcltransporte.com.br>
2025-06-12 16:08:42 -0300

    correções

--- src/utils/getDictionary.js
+++ src/utils/getDictionary.js
@@ -2,10 +2,10 @@
 import 'server-only'
 
 const dictionaries = {
-  pt: () => import('@/data/dictionaries/pt.json').then(module => module.default),
-  en: () => import('@/data/dictionaries/en.json').then(module => module.default),
-  fr: () => import('@/data/dictionaries/fr.json').then(module => module.default),
-  ar: () => import('@/data/dictionaries/ar.json').then(module => module.default)
+  pt: () => import('@/sidebar/dictionaries/pt.json').then(module => module.default),
+  en: () => import('@/sidebar/dictionaries/en.json').then(module => module.default),
+  fr: () => import('@/sidebar/dictionaries/fr.json').then(module => module.default),
+  ar: () => import('@/sidebar/dictionaries/ar.json').then(module => module.default)
 }
 
 export const getDictionary = async locale => dictionaries[locale]()
