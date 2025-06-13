3f6de8c8462149963ea37a7e4c48bb0eab0b6e84
guilhermetcltransporte <guilherme.venancio@tcltransporte.com.br>
2025-06-13 12:06:45 -0300

    correções

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,10 @@
+/** @type {import('postcss-load-config').Config} */
 const config = {
-  plugins: ["@tailwindcss/postcss"],
-};
+  plugins: {
+    'tailwindcss/nesting': {},
+    tailwindcss: {},
+    autoprefixer: {}
+  }
+}
 
-export default config;
+export default config
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,4 @@
 import tailwindcssLogical from 'tailwindcss-logical'
-
 import tailwindPlugin from './src/@core/tailwind/plugin'
 
 const config = {
